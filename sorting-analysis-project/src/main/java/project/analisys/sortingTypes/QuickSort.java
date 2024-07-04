34f9510d4b847ea9c0688e39c49cc6af08882b72
Gustavo Parcianello Cardona <83283617+gucardona@users.noreply.github.com>
2024-07-04 10:26:54 -0300

    Add files via upload

--- sorting-analysis-project/src/main/java/project/analisys/sortingTypes/QuickSort.java
+++ sorting-analysis-project/src/main/java/project/analisys/sortingTypes/QuickSort.java
@@ -1,15 +1,27 @@
 package project.analisys.sortingTypes;
 
+import java.util.Stack;
+
 public class QuickSort {
     public static <T extends Comparable<? super T>> void quickSort(T[] a) {
-        sort(a, 0, a.length - 1);
+        iterativeSort(a, 0, a.length - 1);
     }
 
-    private static <T extends Comparable<? super T>> void sort(T[] a, int low, int high) {
-        if (low >= high) return;
-        int p = partition(a, low, high);
-        sort(a, low, p - 1);
-        sort(a, p + 1, high);
+    private static <T extends Comparable<? super T>> void iterativeSort(T[] a, int low, int high) {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{low, high});
+
+        while (!stack.isEmpty()) {
+            int[] range = stack.pop();
+            low = range[0];
+            high = range[1];
+
+            if (low >= high) continue;
+
+            int p = partition(a, low, high);
+            if (p - 1 > low) stack.push(new int[]{low, p - 1});
+            if (p + 1 < high) stack.push(new int[]{p + 1, high});
+        }
     }
 
     private static <T extends Comparable<? super T>> int partition(T[] a, int low, int high) {
