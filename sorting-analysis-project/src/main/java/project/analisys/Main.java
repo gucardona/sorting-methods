8a9b48a07a57887226659f26bd429e6a7641cbe0
Gustavo Parcianello Cardona <83283617+gucardona@users.noreply.github.com>
2024-07-04 20:08:25 -0300

    Add files via upload

--- sorting-analysis-project/src/main/java/project/analisys/Main.java
+++ sorting-analysis-project/src/main/java/project/analisys/Main.java
@@ -6,65 +6,84 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
+        int[] sizes = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
+        String[] sortMethods = {"BubbleSort", "InsertionSort", "SelectionSort", "HeapSort", "ShellSort", "MergeSort", "QuickSort"};
+        System.out.println("### Sorting Algorithms Analysis ###");
 
-        int[] sizes = { 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536 };
-        String[] sortMethods = { "BubbleSort", "InsertionSort", "SelectionSort", "HeapSort", "ShellSort", "MergeSort", "QuickSort" };
-        System.out.println( "### Sorting Algorithms Analisys ###" );
+        // Print table header
+        System.out.printf("%-15s %-20s %-15s %-15s %-20s %-20s\n", "Array Size", "Array Type", "Sorting Algorithm", "Mean (ns)", "Variance (ns^2)", "Std Deviation (ns)");
+        System.out.println("-------------------------------------------------------------------------------");
 
-        for ( int size : sizes ) {
-            Integer[] crescentArray = ArrayGenerator.generateCrescentArray( size );
-            Integer[] decrescentArray = ArrayGenerator.generateDecrescentArray( size );
-            Integer[] randomArray = ArrayGenerator.generateRandomArray( size );
-            Integer[] randomArrayWithRepeats = ArrayGenerator.generateRandomArrayWithRepeats( size );
+        for (int size : sizes) {
+            Integer[] crescentArray = ArrayGenerator.generateCrescentArray(size);
+            Integer[] decrescentArray = ArrayGenerator.generateDecrescentArray(size);
+            Integer[] randomArray = ArrayGenerator.generateRandomArray(size);
+            Integer[] randomArrayWithRepeats = ArrayGenerator.generateRandomArrayWithRepeats(size);
 
-            System.out.println( "Array size: " + size );
+            List<Integer[]> arrays = Arrays.asList(crescentArray, decrescentArray, randomArray, randomArrayWithRepeats);
 
-            for ( String sortMethod : sortMethods ) {
-                long totalTime = 0;
+            for (Integer[] arr : arrays) {
+                for (String sortMethod : sortMethods) {
+                    long[] executionTimes = new long[10];
 
-                List<Integer[]> arrays = Arrays.asList( crescentArray, decrescentArray, randomArray, randomArrayWithRepeats );
-
-                for ( Integer[] arr : arrays ) {
-                    for ( int i = 0; i < 10; i++ ) {
-                        Integer[] arrCopy = Arrays.copyOf( arr, arr.length );
+                    for (int i = 0; i < 10; i++) {
+                        Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
 
                         long startTime = System.nanoTime();
-                        switch ( sortMethod ) {
+                        switch (sortMethod) {
                             case "BubbleSort":
-                                BubbleSort.bubbleSort( arrCopy );
+                                BubbleSort.bubbleSort(arrCopy);
                                 break;
                             case "InsertionSort":
-                                InsertionSort.insertionSort( arrCopy );
+                                InsertionSort.insertionSort(arrCopy);
                                 break;
                             case "SelectionSort":
-                                SelectionSort.selectionSort( arrCopy );
+                                SelectionSort.selectionSort(arrCopy);
                                 break;
                             case "HeapSort":
-                                HeapSort.heapSort( arrCopy );
+                                HeapSort.heapSort(arrCopy);
                                 break;
                             case "ShellSort":
-                                ShellSort.shellSort( arrCopy );
+                                ShellSort.shellSort(arrCopy);
                                 break;
                             case "MergeSort":
-                                MergeSort.mergeSort( arrCopy );
+                                MergeSort.mergeSort(arrCopy);
                                 break;
                             case "QuickSort":
-                                QuickSort.quickSort( arrCopy );
+                                QuickSort.quickSort(arrCopy);
                                 break;
                         }
                         long endTime = System.nanoTime();
-                        totalTime += ( endTime - startTime );
+                        executionTimes[i] = endTime - startTime;
                     }
-                    System.out.println( "### " + sortMethod + " total time for 10 executions: " + totalTime + " ns for array generation type: " + checkArrayType( arrays.indexOf( arr ) ) + " ###" );
+
+                    // Calculate mean
+                    double sum = 0;
+                    for (long time : executionTimes) {
+                        sum += time;
+                    }
+                    double mean = sum / executionTimes.length;
+
+                    // Calculate variance
+                    double variance = 0;
+                    for (long time : executionTimes) {
+                        variance += Math.pow(time - mean, 2);
+                    }
+                    variance /= executionTimes.length;
+
+                    // Calculate standard deviation
+                    double stdDeviation = Math.sqrt(variance);
+
+                    // Print table row
+                    System.out.printf("%-15d %-20s %-20s %-15.2f %-15.2f %-20.2f\n", size, checkArrayType(arrays.indexOf(arr)), sortMethod, mean, variance, stdDeviation);
                 }
-                System.out.println();
             }
         }
     }
 
-    private static String checkArrayType( int index ) {
-        return switch ( index ) {
+    private static String checkArrayType(int index) {
+        return switch (index) {
             case 0 -> "Crescent";
             case 1 -> "Decrescent";
             case 2 -> "Random";
